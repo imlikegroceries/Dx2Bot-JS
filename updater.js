798b1f67c2c9fbf331e48865f5848ce0df16b679
imlikegroceries <107961604+imlikegroceries@users.noreply.github.com>
2022-10-26 13:22:17 -0700

    github

---
+++ updater.js
@@ -0,0 +1,11 @@
+
+const {readWiki} = require('./tierlistReader');
+const {readBanners} = require('./bannerCSV');
+
+module.exports = async function updateBot() {
+    const tierList = await readWiki();
+    const banner = await readBanners(20);
+
+    console.log({tierList, banner});
+    return {tierList, banner};
+}
